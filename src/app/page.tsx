b657712cb902f3db73cdbdc5bc612ee4350f513f
Victor Hugo Forbes <vhforbes@MacBook-Air-de-Victor.local>
2024-04-29 08:54:44 -0300

    feat: daisy ui setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,8 @@ import TestAtomComp from "@/components/01-atoms/TestAtomComponent";
 export default function Home() {
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="text-2xl">Home Page</div>
+      <div className="text-2xl text-primary">Home Page</div>
+      <div className="btn btn-secondary">button</div>
       <TestAtomComp />
     </main>
   );
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,4 +1,5 @@
 import type { Config } from "tailwindcss";
+import daisyui from "daisyui";
 
 const config: Config = {
   content: [
@@ -15,6 +16,33 @@ const config: Config = {
       },
     },
   },
-  plugins: [],
+  plugins: [daisyui],
+
+  daisyui: {
+    themes: [
+      {
+        dark: {
+          primary: "#06E5AD",
+          secondary: "#19D6E5",
+          accent: "#FD0A62",
+          neutral: "#414657",
+          "base-100": "#4D4E5A",
+          "base-200": "#313541",
+          "base-300": "#1C1E29",
+          info: "#325E9F",
+          success: "#19D6E5",
+          warning: "#E2A400",
+          error: "#FC632C",
+        },
+      },
+    ], // false: only light + dark | true: all themes | array: specific themes like this ["light", "dark", "cupcake"]
+    darkTheme: "dark", // name of one of the included themes for dark mode
+    base: true, // applies background color and foreground color for root element by default
+    styled: true, // include daisyUI colors and design decisions for all components
+    utils: true, // adds responsive and modifier utility classes
+    prefix: "", // prefix for daisyUI classnames (components, modifiers and responsive class names. Not colors)
+    logs: true, // Shows info about daisyUI version and used config in the console when building your CSS
+    themeRoot: ":root", // The element that receives theme color CSS variables
+  },
 };
 export default config;
